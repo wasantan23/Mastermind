ccb3de1be8ff2996189e2a3f65ea95734c4d47bc
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2022-02-02 23:58:43 -0500

    Feb 2 - Working code - need to add comments

--- Tester.java
+++ Tester.java
@@ -1,24 +1,36 @@
-import java.util.Scanner;
+//Tanisha Wasan 
+//import java.util.Scanner;
 
 public class Tester {
 public static void main (String [] args) {
 
 Mastermind m1 = new Mastermind();
 
-m1.printBoard();
+System.out.println(" Welcome to the Mastermind game! \n In this game, you are going to guess the color of the hidden pegs that \n the computer has placed. \n There are NO repeats in this game");
 
 m1.buildBoard();
 
+m1.printBoard();
+
+
 //make an array list 
 //you don't need to do 4
-char [] guess = new char [2];
+/*char [] guess = new char [4];
 Scanner scan = new Scanner (System.in); 
 
 for(int x = 0; x < 4; x ++) {
-	System.out.println("Take your guess!");
-guess[x] =scan.next().charAt(0);
+	System.out.println(" Take your guess. B for blue, Y for yellow, P for purple, R for red, O for orange and G for green. ");
+	guess[x] =scan.next().charAt(0);
+}
+*/
+
+//m1.addGuess(guess);
+
+m1.computerGuess();
+m1.userGuess();
+
 }
 }
-}
+
 
 
